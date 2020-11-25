bd8ce72cd9c8bdcd0fffb717ee46759a3a49941b
Alexander Kosik <alexander.kosik@aareal-bank.com>
2020-11-25 16:19:01 +0100

    WIP2

--- probabilities/probs.py
+++ probabilities/probs.py
@@ -6,33 +6,55 @@ SCORE_GR_STRASSE = 40
 SCORE_KL_STRASSE = 30
 SCORE_FULL_HOUSE = 25
 
-def kniffel(first):
-    probs, _ = pasch(first, 6)
+NUMBER_OF_DICES = 5
+
+def kniffel(first_roll):
+    probs, _ = pasch(first_roll, 5)
     return probs, SCORE_KNIFFEL
 
-def pasch(first, kind_of):
+def pasch(first_roll, kind_of):
     '''Errechnet die Pasch Wahrscheinlichkeiten mit der Gewinnsumme'''
 
-    c = Counter((int(x) for x in first))
+    c = Counter((int(x) for x in first_roll))
     if kind_of not in set(range(1,7)):
         raise ValueError("Nur Zahlen von 1-6 als Argument erlaubt")
     number, amount = c.most_common(1)[0]
     needed = kind_of - amount
-    gains = number * kind_of + (6-kind_of) * AVG_SCORE
-    probability = 100 if needed <= 0 else pow(1/6, needed) * 2
-    return round(probability, 3), gains
+    gains = number * kind_of + (NUMBER_OF_DICES - kind_of) * AVG_SCORE
+    probability = 1 if needed <= 0 else pow(1/6, needed) * 2
+    return probability, gains
 
-def gleiche(first, kind_of):
+def gleiche(first_roll, kind_of):
     '''Errechnet die Wahrscheinlichkeiten für mind. 3 Gleiche'''
-    c = Counter((int(x) for x in first))
+    c = Counter((int(x) for x in first_roll))
     if kind_of not in set(range(1, 7)):
         raise ValueError("Nur Zahlen von 1-6 als Argument erlaubt")
     number = kind_of
     amount = c[kind_of]
     needed = kind_of - amount
-    gains = number * kind_of + (6 - kind_of) * AVG_SCORE
-    probability = 100 if needed <= 0 else pow(1 / 6, needed) * 2
-    return round(probability, 3), gains
+    gains = number * kind_of + (NUMBER_OF_DICES - kind_of) * AVG_SCORE
+    probability = 1 if needed <= 0 else pow(1 / 6, needed) * 2
+    return probability, gains
 
-gleiche("666655", 6)
+def kl_strasse(first_roll):
+    '''Errechnet die Wahrscheinlichkeiten für eine kleine Straße'''
+    c = Counter((int(x) for x in first_roll))
+    num_unique = len(c.most_common())
+    needed = 5 - num_unique
+    probability = 1 if needed <= 0 else pow(1 / 6, needed) * 2 * (NUMBER_OF_DICES - num_unique)
+    return probability, SCORE_KL_STRASSE
 
+def gr_strasse(first_roll):
+    '''Errechnet die Wahrscheinlichkeiten für eine kleine Straße'''
+    c = Counter((int(x) for x in first_roll))
+    num_unique = len(c.most_common())
+    needed = NUMBER_OF_DICES - num_unique
+    probability = 1 if needed <= 0 else pow(1 / 6, needed) * 2 * (NUMBER_OF_DICES - num_unique)
+    return probability, SCORE_GR_STRASSE
+
+def full_house(first_roll):
+    c = Counter((int(x) for x in first_roll))
+    num_unique = len(c.most_common())
+    needed = 6 - num_unique
+    probability = 1 if needed <= 0 else pow(1 / 6, needed) * 2 * (NUMBER_OF_DICES - num_unique)
+    return probability, SCORE_GR_STRASSE
--- tests/unittests.py
+++ tests/unittests.py
@@ -1,20 +1,30 @@
 import unittest
-from collections import Counter
 from probabilities import probs
 
 class TestProbabilities(unittest.TestCase):
     def test_pasch(self):
-        prob, gains_6er = probs.pasch("666433", 3)
-        self.assertEqual(prob, 100)
-        self.assertEqual(gains_6er, 28.5)
+        prob, gains_6er = probs.pasch("66643", 3)
+        self.assertEqual(prob, 1)
+        self.assertEqual(gains_6er, 25)
 
-        prob, gains_5er = probs.pasch("555433", 3)
-        self.assertEqual(prob, 100)
-        self.assertEqual(gains_5er, 25.5)
+        prob, gains_5er = probs.pasch("55543", 3)
+        self.assertEqual(prob, 1)
+        self.assertEqual(gains_5er, 22)
 
     def test_kniffel(self):
-        prob, gain = probs.kniffel("666633")
-        print(prob, gain)
+        prob, gain = probs.kniffel("66633")
+        self.assertEqual(prob, 0.056)
+
+    def test_kl_strasse(self):
+        prob, gain = probs.kl_strasse("12334")
+        self.assertAlmostEqual(prob, 1/3)
+        self.assertEqual(gain, 30)
+
+    def test_gr_strasse(self):
+        prob, gain = probs.gr_strasse("12343")
+        self.assertAlmostEqual(prob, 1/3)
+        self.assertEqual(gain, 40)
+
 
 if __name__ == '__main__':
     unittest.main()
